f490a7e7b4e7b80235b75e10726c01b4810cbf90
Jenna Eagleson <eagleson.jenna@gmail.com>
2025-08-26 16:56:39 -0500

    completed one viz enterprise voluntary turnover monthly historical comparison

--- output_visuals.R
+++ output_visuals.R
@@ -1,3 +1,6 @@
+library(ggtext)
+library(hrbrthemes)
+
 monthly_turnover_data <- map(grouping_vars, ~ calculate_voluntary_turnover_monthly(pa_at, .x)) |> 
   set_names(output_sheet_names)
 
@@ -10,14 +13,14 @@ combined_turnover_data <-
 vt_output_name_py <- "outputs/Professional Voluntary Turnover Rate Monthly 2024-01-01 to 2024-12-31.xlsx"
 
 # get sheet names
-vt_sheet_names_py <- excel_sheets(vt_output_name_py)
+vt_sheet_names_py <- readxl::excel_sheets(vt_output_name_py)
 
 # only get sheet names with metrics, not headcount or term sheets
 vt_sheet_names_py <- vt_sheet_names_py[1:6]
 
 # pull in each of the sheets
 vt_data_py <- vt_sheet_names_py |> 
-    map(~ read_excel(vt_output_name_py, sheet = .x) |> 
+    map(~ readxl::read_excel(vt_output_name_py, sheet = .x) |> 
     mutate(sheet = .x))
 
 # bind together
@@ -37,24 +40,46 @@ all_turnover_data <- combined_turnover_data |>
 )) |> 
   mutate(year = as.factor(year))
 
+py_turnover <- all_turnover_data |> 
+  filter(month == "ytd", year == "2024", grouping_level == "Enterprise") |> 
+  pull(voluntary_turnover)
+
+turnover_goal <- (py_turnover * 0.90) / 12
+
 # visuals
+
+# vt historical
 viz_vt_historical <- all_turnover_data |> 
   filter(month != "ytd" & !str_detect(month, "Q")) |> 
   filter(grouping_level == "Enterprise") |> 
   mutate(month = factor(month, levels = month.abb, ordered = TRUE))
 
+label_most_recent <- viz_vt_historical |> 
+  filter(year == 2025) |> 
+  slice_max(order_by = month, n = 1)
+
 viz_vt_historical |> 
   ggplot(aes(x = month, y = voluntary_turnover, group = year, color = year)) + 
-  geom_line(size = 1.2) +
-  geom_point(size = 1.2) +
+  geom_hline(yintercept = turnover_goal, color = r_gray, size = 1.5, alpha = 0.5) +
+  geom_line(size = 1.5) +
+  geom_point(size = 3) +
+  geom_text(data = label_most_recent, 
+    aes(label = scales::percent(voluntary_turnover, accuracy = 0.1)),
+  hjust = -0.2, vjust = 1.2) +
   scale_y_continuous(labels = scales::percent_format(accuracy = 0.1), limits = c(0, 0.015)) +
-  scale_color_manual(values = c("2024" = r_green, "2025" = r_darkblue)) +
+  scale_color_manual(values = c("2024" = r_darkblue, "2025" = r_green)) +
   labs(
-    title = "Monthly Voluntary Turnover vs. 2024",
+    title = "<span style='color:#00A651;'>Monthly Voluntary Turnover</span> vs. <span style='color:#003865;'>2024</span>",
     x = NULL,
     y = NULL
   ) +
-  theme_minimal(base_size = 14) +
+  theme_ipsum() +
   theme(
-    panel.grid.minor = element_blank()
+    plot.title = element_markdown(),
+    plot.margin = margin(2, 2, 2, 2),
+    legend.position = "none",
+    panel.grid.minor = element_blank(),
+    panel.grid.major.x = element_blank()
   )
+
+ggsave("outputs/visuals/enterprise_vt_monthly_historical.png", width = 6.73, height = 2.1, units = "in")
